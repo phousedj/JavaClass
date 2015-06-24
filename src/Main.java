079e0ab2f6dbbf8ad690dd97f0cfde66527dd212
Chris Leake-Skywell <lists@oursgathering.org>
2015-06-23 21:00:56 -0700

    initial checkin

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Created by chris on 6/23/15.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        byte a = 1;
+        byte b = 2;
+        int c;
+        c = a+b;
+
+        System.out.println(c);
+
+    }
+}
+
+
