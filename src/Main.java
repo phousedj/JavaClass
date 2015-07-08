8e8d5523c917d3a4013942653a332e395d588a6b
Chris Leake-Skywell <lists@oursgathering.org>
2015-07-07 21:03:26 -0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,10 @@ public class Main
     public static void main(String[] args)
     {
 
-        byte a = 1;
-        byte b = 2;
-        int c;
-        c = a+b;
+        final byte a = 2;
+        final byte b = 2;
+        byte c = 0 ;
+        c = a + b;
 
         System.out.println(c);
 
